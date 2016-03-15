6511777573bd209fd17366ce770e0062da103f87
sraskin <shuvho.91@gmail.com>
2016-03-16 01:38:57 +0600

    association between user and docs

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160315192305) do
+ActiveRecord::Schema.define(version: 20160315193004) do
 
   create_table "docs", force: :cascade do |t|
     t.string   "title",      limit: 255
     t.text     "content",    limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.integer  "user_id",    limit: 4
   end
 
   create_table "users", force: :cascade do |t|
